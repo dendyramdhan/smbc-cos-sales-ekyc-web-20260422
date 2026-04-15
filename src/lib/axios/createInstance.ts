2c826b61e17a226b431f12cccd9ae4b45d764943
Aziz Ruri Suparman <it250304@btpn.com>
2026-04-15 08:18:15 +0100

    fix(axios): use API_CONFIG for timeout in createAxiosInstance

--- src/lib/axios/createInstance.ts
+++ src/lib/axios/createInstance.ts
@@ -1,5 +1,6 @@
 import axios, { type AxiosInstance } from 'axios';
 import { type AxiosInstanceConfig } from './types';
+import { API_CONFIG } from '@/constants/apiConfig';
 
 export const createAxiosInstance = (config: AxiosInstanceConfig = {}): AxiosInstance => {
   const { label, withAuth, withLogging, ...axiosConfig } = config;
@@ -8,7 +9,7 @@ export const createAxiosInstance = (config: AxiosInstanceConfig = {}): AxiosInst
   void withLogging;
 
   return axios.create({
-    timeout: 15000,
+    timeout: API_CONFIG.TIMEOUT,
     headers: { 'Content-Type': 'application/json' },
     ...axiosConfig
   });
