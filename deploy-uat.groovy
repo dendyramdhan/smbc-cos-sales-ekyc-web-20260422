283bfac2488131f74f4e824f6044c372845f09e4
hilman.trihatmojo <mohammed.hilmantrihatmojo@btpn.com>
2026-04-16 14:30:11 +0700

    [hilman] fix deploy groovy

--- deploy-uat.groovy
+++ deploy-uat.groovy
@@ -4,24 +4,24 @@ node (){
     deployDescription deployDesc: deployDesc
     dir (env.BUILD_ID){
         def serviceName = "ekyc-web"
-        def nsService = "Customer-Administrative-Platform/ekyc-web"
-        def dockerRegistry = "docker-registry-default.apps.dev.corp.btpn.co.id"
-        def dockerNexus = "nexus.corp.bankbtpn.co.id:50001"
-        def domain = "apps.dev.corp.btpn.co.id"
-        def ocpUrl = "https://ocp.dev.corp.btpn.co.id:8443"
+        def nsService = "customer-administrative-platform/ekyc-web"
+        def dockerRegistry = "default-route-openshift-image-registry.apps.ms.dev.corp.btpn.co.id"
+        def dockerNexus = "nexus.corp.bankbtpn.co.id:50003"
+        def domain = "apps.ms-bm.dev.corp.btpn.co.id"
+        def ocpUrl = "https://api.ms-bm.dev.corp.btpn.co.id:6443"
         def namespace = "customer-administrative-platform-uat"
-        def ocpCredential = "OCP_DEV"
+        def ocpCredential = "OBM_DEV"
+        def replica = 1
 
         currentBuild.displayName = "#${BUILD_NUMBER}, ${version}"
 
         stage ("tag & push"){
             dockerPull image: "${dockerNexus}/${nsService}:${version}"
-            dockerTag source: "${dockerNexus}/${nsService}:${version}", to: "${dockerRegistry}/${namespace}/ekyc-web:${version}"
         }
         
         stage ("deploy"){
             checkout scm: [$class: 'GitSCM', userRemoteConfigs: [[credentialsId: 'GIT_CRED', url: "https://git.ecommchannels.com/Customer-Administrative-Platform/ekyc-web.git"]], branches: [[name: "refs/tags/${version}"]]]  
-            ocpDeploy session: "${namespace}-ekyc-web-${BUILD_NUMBER}", namespace: namespace, serviceName: serviceName, ocpUrl: ocpUrl, ocpCredential: ocpCredential, dockerRegistry: dockerRegistry, image: "${dockerRegistry}/${namespace}/ekyc-web:${version}", template: "deploy/template.yml", variable: "deploy/uat.env", parameters: "domain=apps.dev.corp.btpn.co.id namespace=${namespace} version=${version}"
+            ocpDeploy session: "${namespace}-ekyc-web-${BUILD_NUMBER}", namespace: namespace, serviceName: serviceName, ocpUrl: ocpUrl, ocpCredential: ocpCredential, dockerRegistry: dockerRegistry, image: "${dockerNexus}/${nsService}:${version}", template: "deploy/template.yml", variable: "deploy/uat.env", parameters: "domain=${domain} namespace=${namespace} version=${version} replica=${replica}"
         }
     }
 }
